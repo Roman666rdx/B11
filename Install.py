4e35cb4829e0fa57a92da36e21d4dc1406f29bae
Roman666rdx <96844040+Roman666rdx@users.noreply.github.com>
2021-12-30 02:31:45 +0600

    Create Install.py

---
+++ Install.py
@@ -0,0 +1,12 @@
+ B11
+#pkg update && pkg upgrade && pkg install python && pkg install python2 && pkg install git && pkg install pip && pkg install pip2
+#git clone:github.com/Roman666rdx/B11/new/main?readme=1
+#pip2 install requests
+#pip2 install mechanize
+#ls && cd b11 && ls
+
+#python2 b11.py
+
+#TOOL USER : (knock me on facebook)
+
+#TOOL PASS : (knock me on facebook)
